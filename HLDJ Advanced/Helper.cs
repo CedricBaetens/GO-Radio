43fe3d137f01f9adf8766b42928f18400d766668
Baellon <cedric.baetens@gmail.com>
2015-12-06 22:04:18 +0100

    First version of program, get songs in folder + read/write json with category

---
+++ HLDJ Advanced/Helper.cs
@@ -0,0 +1,21 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HLDJ_Advanced
+{
+    static class Helper
+    {
+        public static List<Sound> GetAllSounds(string path)
+        {
+            string[] rawSounds = System.IO.Directory.GetFiles(path, "*.*", System.IO.SearchOption.AllDirectories);
+
+            List<Sound> sounds = rawSounds.Select(rawSound => new Sound(rawSound, path)).ToList();
+
+            return sounds;
+        }
+    }
+}
--- HLDJ Advanced/MainWindow.xaml.cs
+++ HLDJ Advanced/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +14,89 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Kennedy.ManagedHooks;
+using Newtonsoft.Json;
+using PropertyChanged;
 
 namespace HLDJ_Advanced
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    /// 
+    [ImplementPropertyChanged]
     public partial class MainWindow : Window
     {
+
+        private KeyboardHook keyboardHook;
+
+        private const string hldjPath = @"C:\Users\Baellon\Desktop\HLDJ\audio";
+
+        public ObservableCollection<Category> Categories { get; set; }
+
+
+        public ObservableCollection<Sound> AllSounds { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
         }
+
+        private void KeyboardHook_KeyboardEvent(KeyboardEvents kEvent, System.Windows.Forms.Keys key)
+        {
+        }
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            // Instanciate
+            Categories = new ObservableCollection<Category>();
+
+            // Keyboard hool
+            keyboardHook = new KeyboardHook();
+            keyboardHook.KeyboardEvent += KeyboardHook_KeyboardEvent;
+            keyboardHook.InstallHook();
+
+            // Read Json
+            var jsonRead = File.ReadAllText("data_format.json");
+            var jsonArray = JsonConvert.DeserializeObject<List<Sound>>(jsonRead);
+
+            foreach (var jsonItem in jsonArray)
+            {
+                // Place item in correct category
+                bool found = false;
+                foreach (var category in Categories)
+                {
+                    if (category.Name == jsonItem.Category)
+                    {
+                        category.Sounds.Add(jsonItem);
+                        found = true;
+                    }
+                }
+
+                // Create category if its new
+                if (!found)
+                {
+                    Categories.Add(new Category()
+                    {
+                        Name = jsonItem.Category
+                    });
+                    Categories[Categories.Count - 1].Sounds.Add(jsonItem);
+                }
+            }
+
+
+            // Tree structure
+            //AllSounds = new ObservableCollection<Sound>(Helper.GetAllSounds(hldjPath));
+            //var json = JsonConvert.SerializeObject(AllSounds, Formatting.Indented);
+            //File.WriteAllText("test.txt", json);
+
+
+
+            //AllSounds = JsonConvert.DeserializeObject<List<Sound>>(jsonRead);
+
+
+
+            int a = 0;
+        }
+
+
     }
 }
