850a08ba68673feeb9b78f73bd5f1c6758b1def3
Cedric Baetens <cedric.baetens@intation.eu>
2015-12-24 14:00:40 +0100

    Added basic trim func

--- CSGO Radio/Classes/Category.cs
+++ CSGO Radio/Classes/Category.cs
@@ -49,10 +49,10 @@ namespace CSGO_Radio.Classes
         }
         private void RecalculateIds()
         {
-            //for (int i = 0; i < Sounds.Count; i++)
-            //{
-            //    Sounds[i].Id = StartId + i;
-            //}
+            for (int i = 0; i < Sounds.Count; i++)
+            {
+                Sounds[i].Id = StartId + i;
+            }
         }
         private void EditCategory()
         {
@@ -61,11 +61,19 @@ namespace CSGO_Radio.Classes
             cw.ShowDialog();
         }
 
+
+        private void TrimSound()
+        {
+            NAudioHelper.TrimWavFile(SelectedSound.Path, string.Format("{0}/{1}(trimmed).wav", SelectedSound.Directory, SelectedSound.Name), new TimeSpan(0, 0, 5), new TimeSpan(0, 0, 5));
+        }
+
+
         // Command Binding
         public ICommand CommandRemoveSound { get { return new RelayCommand(RemoveSound); } }
         public ICommand CommandRecalculateIds { get { return new RelayCommand(RecalculateIds); } }
         public ICommand CommandEditCategory { get { return new RelayCommand(EditCategory); } }
-        
- 
+        public ICommand CommandTrimSound { get { return new RelayCommand(TrimSound); } }
+
+
     }
 }
