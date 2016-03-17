2e9db7ed7b8284ece3cb4194992f93b480a16ab3
Baellon <cedric.baetens@gmail.com>
2016-03-17 22:18:48 +0100

    small fix to console input

--- GO Radio/Classes/Cfg.cs
+++ GO Radio/Classes/Cfg.cs
@@ -17,6 +17,7 @@ namespace GO_Radio.Classes
                 content.Add("alias bs_play_off \"-voicerecord; voice_inputfromfile 0; voice_loopback 0; alias bs_play bs_play_on\"");
                 content.Add("alias tts \"condump;\"");
                 content.Add("alias la \"exec radio_categorylist\"");
+                content.Add("alias radiounbind \"exec radio_unbindall\"");
                 content.Add("con_logfile radiolog.txt");
                 content.Add(string.Format("bind {0} bs_play", keybindings.Keys[(int)KeyBinder.KeyTranslation.PlayPauze].KeyCsGo));
                 content.Add(string.Format("bind {0} bs_play", keybindings.Keys[(int)KeyBinder.KeyTranslation.PlayStop].KeyCsGo));
@@ -30,9 +31,9 @@ namespace GO_Radio.Classes
                 int count = 0;
                 foreach (var category in list.Categories)
                 {                                    
-                    content.Add(string.Format("echo {2}: {0}: {1}", category.Name, category.Range, count));
+                    content.Add(string.Format("echo c{2}: {0}: {1}", category.Name, category.Range, count));
                     SongList(category,count);
-                    content.Add(string.Format("alias {0} \"exec radio_category_soundlist{0}.cfg\"", count++));
+                    content.Add(string.Format("alias c{0} \"exec radio_category_soundlist{0}.cfg\"", count++));
                 }
 
                 File.WriteAllLines(ProgramSettings.PathCsgo + "\\csgo\\cfg\\radio_categorylist.cfg", content);
@@ -44,9 +45,9 @@ namespace GO_Radio.Classes
                 int count = 0;
                 foreach (var sound in category.Sounds)
                 {
-                    content.Add(string.Format("echo {2}: {0}: {1}", sound.Id.ToString("0000"), sound.Name, count));
+                    content.Add(string.Format("echo t{2}: {0}: {1}", sound.Id.ToString("0000"), sound.Name, count));
                     SongLoad(sound);
-                    content.Add(string.Format("alias {0} \"exec radio_category_soundload{1}.cfg\"", count++, sound.Id.ToString("0000")));
+                    content.Add(string.Format("alias t{0} \"exec radio_category_soundload{1}.cfg\"", count++, sound.Id.ToString("0000")));
                 }
 
                 File.WriteAllLines(string.Format("{0}\\csgo\\cfg\\radio_category_soundlist{1}.cfg", ProgramSettings.PathCsgo,index), content);
@@ -58,6 +59,25 @@ namespace GO_Radio.Classes
                 content.Add(string.Format("echo load {0};condump; ", sound.Id.ToString("0000")));
 
                 File.WriteAllLines(string.Format("{0}\\csgo\\cfg\\radio_category_soundload{1}.cfg", ProgramSettings.PathCsgo, sound.Id.ToString("0000")), content);
+            }
+            private static void UnbindAll(int count)
+            {
+                //List<string> content = new List<string>();
+
+                //for (int i = 0; i < 100; i++)
+                //{
+                //    content.Add("unbind bs_play bs_play_on");
+                //}
+                
+                //content.Add("alias bs_play_on \"alias bs_play bs_play_off; voice_inputfromfile 1; voice_loopback 1; +voicerecord\"");
+                //content.Add("alias bs_play_off \"-voicerecord; voice_inputfromfile 0; voice_loopback 0; alias bs_play bs_play_on\"");
+                //content.Add("alias tts \"condump;\"");
+                //content.Add("alias la \"exec radio_categorylist\"");
+                //content.Add("con_logfile radiolog.txt");
+                //content.Add(string.Format("bind {0} bs_play", keybindings.Keys[(int)KeyBinder.KeyTranslation.PlayPauze].KeyCsGo));
+                //content.Add(string.Format("bind {0} bs_play", keybindings.Keys[(int)KeyBinder.KeyTranslation.PlayStop].KeyCsGo));
+
+                //File.WriteAllLines(ProgramSettings.PathCsgo + "\\csgo\\cfg\\radio_unbindall.cfg", content);
             }
         }
         public static class Remove
